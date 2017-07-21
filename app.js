36549b26d3c85fb294c29fd4c78fa7fd53c101ea
Diego Solano <dsolano@assist.com.co>
2017-07-21 19:42:19 +0000

    Aprobado

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
